6b737d3d7b4f3a42839a110a160ad0334d4d6d0e
가동재 <masterforce999@naver.com>
2024-11-22 23:02:55 +0900

    피드백

--- src/componentes/Header/Header.jsx
+++ src/componentes/Header/Header.jsx
@@ -1,29 +1,57 @@
-import Style from './style.module.css'
-import { useLocation } from 'react-router-dom';
-const Header = ({title, litleTitle}) =>{
+import Style from "./style.module.css";
+import { useLocation, Link } from "react-router-dom";
+const Header = ({ title, litleTitle }) => {
   const { pathname } = useLocation();
+
+  const btnArr = [
+    {
+      className: "search-bell",
+      src: "/image/searchBell.png",
+      alt: "bell",
+    },
+    {
+      className: "search-moon",
+      src: "/image/searchmoon.png",
+      alt: "moon",
+    },
+    {
+      className: "search-icon",
+      src: "/image/searchIcon.png",
+      alt: "Icon",
+    },
+    {
+      className: "search-people",
+      src: "/image/searchPeople.png",
+      alt: "people",
+    },
+  ];
+
   return (
-    <header className={Style.mainHeader}>
-      <div>
-        <div className={Style.headerDiv}>{litleTitle}</div>
-        <h2 className={Style.header}>{title}</h2>
+    <header className={Style["main-header"]}>
+      <div className={Style["haeder-breadcrumb"]}>
+        <div>{litleTitle}</div>
+        <h2>{title}</h2>
       </div>
-      
-      <div className={pathname === "/NFTMarketplacePage" ? Style.searchBoxPlus : Style.searchBox}>
-        <input className={Style.searchInput} type="search" placeholder='Search'/>
+
+      <div className={Style["haeder-search"]}>
+        <input type="search" placeholder="Search" />
         {pathname === "/NFTMarketplacePage" && (
-          <div className={Style.searchETF}>
-            <img className={Style.searchIMGETH} src="/image/searchETH.png" alt="ETH" />
+          <div className={Style["search-etf"]}>
+            <img src="/image/searchETH.png" alt="ETH" />
             1,924 ETH
           </div>
         )}
-        <img className={Style.searchImgBell} src="/image/searchBell.png" alt="bell" />
-        <img className={Style.searchImgMoon} src="/image/searchmoon.png" alt="moon" />
-        <img className={Style.searchImgIcon} src="/image/searchIcon.png" alt="Icon" />
-        <img className={Style.searchImgPeople} src="/image/searchPeople.png" alt="people" />
+
+        {btnArr.map(({ className, alt, src }, index) => {
+          return (
+            <Link to="" key={index}>
+              <img className={Style[className]} src={src} alt={alt} />
+            </Link>
+          );
+        })}
       </div>
     </header>
-  )
-}
+  );
+};
 
 export default Header;
