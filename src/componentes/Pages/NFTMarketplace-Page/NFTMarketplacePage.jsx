2989724f613966cb6af99e851db0bd168f1bc983
Kurman11 <skedyd45@naver.com>
2024-11-26 23:05:29 +0900

    filter

--- src/componentes/Pages/NFTMarketplace-Page/NFTMarketplacePage.jsx
+++ src/componentes/Pages/NFTMarketplace-Page/NFTMarketplacePage.jsx
@@ -1,66 +1,48 @@
 import { useState } from "react";
-
 import SubHeader from "./SubHeader/SubHeader";
-
 import NFTcard from "./NFTcard/NFTcard.jsx";
-import cardImage from "./image/Card.png";
+// import cardImage from "./image/Card.png";
 import GropImage from "./image/Group.png";
+import MockItems from './mock/dummy.js'
+
 
 export default function NFTMarketplacePage() {
-  const [tab, setTab] = useState("Art");
+  // const [tab, setTab] = useState("Art");
+  const [order, setItems] = useState(MockItems.filter((item) => item.type === "Art"));
+  const [mockData] = useState(MockItems)
+  // console.log(order)
 
   const btnArr = [
-    {
-      title: "Art",
-    },
-    {
-      title: "Music",
-    },
-    {
-      title: "Collectibles",
-    },
-    {
-      title: "Sports",
-    },
+    {title: "Art"},
+    {title: "Music"},
+    {title: "Collectibles"},
+    {title: "Sports"},
   ];
 
   const handleTab = (value) => {
-    setTab(value);
+    const typeFilter = mockData.filter((od) => od.type === value)
+    setItems(typeFilter);
   };
 
   const renderTab = () => {
-    switch (tab) {
-      case "Art":
-      default: {
-        return (
-          <div>
-            <NFTcard
-              mainImg={cardImage}
-              alt="sub메인 이미지"
-              firstText="Abstract Colors"
-              peopleimg={GropImage}
-              peopleimgAlAlt="좋아요누른 사람들"
-              firstTextSub="By Esthera Jackson"
-              secondTextSub="Current Bid : 0.91ETH"
-            />
-          </div>
-        );
-      }
-      case "Music": {
-        return <div>Music</div>;
-      }
-      case "Collectibles": {
-        return <div>Collectibles</div>;
-      }
-      case "Sports": {
-        return <div>Sports</div>;
-      }
-    }
+    return order.map(({ title, name, bit, img, isGood }, index) => (
+      <div key={index}>
+        <NFTcard
+          mainImg= {img}
+          alt="sub메인 이미지"
+          firstText={title}
+          peopleimg={GropImage}
+          peopleimgAlt={isGood}
+          firstTextSub={name}
+          secondTextSub={`Current Bid: ${bit}`}
+        />
+      </div>
+    ));
   };
   return (
     <section className="section">
-      <SubHeader tabArr={btnArr} handleTab={handleTab} />
-      {renderTab()}
+        <SubHeader tabArr={btnArr} handleTab={handleTab} />
+        <div>{renderTab()}</div>
     </section>
   );
 }
